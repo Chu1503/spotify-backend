f7d9c449e182a2024b5b59303ce0cf956e35b66b
Chu1503 <shivakash2003@gmail.com>
2024-11-05 21:49:10 +0530

    access token

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://spotify-frontend-sigma.vercel.app", // Vercel frontend URL
+    origin: "https://spotify-frontend-sigma.vercel.app",
     credentials: true,
   })
 );
